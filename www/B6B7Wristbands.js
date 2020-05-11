7780ca911196de5c19f90f2a960a76ba6a0fa5d7
André Grillo <andregrillo@gmail.com>
2020-05-11 12:24:17 +0100

    Wristbands.js fix for Outsystems

--- www/B6B7Wristbands.js
+++ www/B6B7Wristbands.js
@@ -1,7 +1,5 @@
-cordova.define("cordova-plugin-wristbands.B6B7Wristbands", function(require, exports, module) {
                var exec = require('cordova/exec');
                
                exports.setDevice = function (success, error, model, deviceId, command, backgroundTracking) {
                exec(success, error, 'WristbandsPlugin', 'setDevice', [model, deviceId, command, backgroundTracking]); }
-               });
 
